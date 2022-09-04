743c49171939c1664a52be49b204cd73afd11d2f
fahadchowdhary007 <fahadchowdhary007@gmail.com>
2022-09-04 15:40:04 +0530

    Latest changes pushed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
+const cors = require('cors');
 
 const data  = {
     text: "Hello From Backend"
 }
 
 app.use(morgan('dev'));
+app.use(cors());
 app.get('/firstBackend', (req,res) =>{
     res.json(data)
     // res.send('first backend!')
@@ -14,7 +16,7 @@ app.get('/firstBackend', (req,res) =>{
 
 
 
-const port = 8080;
+const port = 8081;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
